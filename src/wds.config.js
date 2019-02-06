f82f4bbd77d2b0ad7fab51c591c3bc6674d4efff
famibee <famibee@gmail.com>
2019-02-06 22:26:55 +0900

    タグリファレンス暫定版公開

---
+++ src/wds.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: `./src/web4webpack.js`,
+	target: 'web',
+	mode: 'development',
+	output: {
+		path: process.cwd() +'/SKYNovel',
+		filename: 'index.js',
+	},
+
+	devServer: {
+		contentBase: './SKYNovel',
+		openPage: 'tag.htm#development=1',
+		watchContentBase: true,
+		open: true
+	},
+};
